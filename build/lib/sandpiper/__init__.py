2262c99537452b55dbd41a0a86e0a5e10405e91d
Nicolas Pucino <44853536+npucino@users.noreply.github.com>
2021-02-19 13:04:29 +1100

    add packaging files
    structure of the folder changed, git config--global core.autocrlf false, new packaging files

---
+++ build/lib/sandpiper/__init__.py
@@ -0,0 +1,8 @@
+__version__='0.0.1'
+from . import dynamics
+from . import hotspot
+from . import labels
+# from . import outils
+# from . import outils
+from . import profile
+from . import volumetrics
